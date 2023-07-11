96804e62ac694b6cb8cb2374436fb268f6b53a7f
Matvey Rudinskiy <brokamp@mail.ru>
2023-07-11 12:42:05 +0300

    Создание хеддера и добавление кнопок входа и регистрации. Добавление обработки нажатия на вход

--- owltasks/src/App.js
+++ owltasks/src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import React from "react";
+import {Header} from "antd/es/layout/layout";
+import {Button, Layout} from "antd";
 
-function App() {
+export default function App() {
+  const navigate = useNavigate();
+
+  const navigateLog = () => {
+    navigate('/login');
+  };
+
   return (
-    <div className="App">
+      <Layout className="layout" style={{padding: "-10px"}}>
+        <div className="App">
+          <Header style={{display: 'flex', alignItems: 'center', backgroundColor: "#423189", justifyContent: 'flex-start'}}>
+            <h1 style={{color: "white"}}>TaskOwl</h1>
+            <Button className="RegButton" style={{marginLeft: 'auto'}}>Регистрация</Button>
+            <Button type="primary" style={{marginLeft: "10px"}} onClick={navigateLog}>Вход</Button>
+          </Header>
 
-    </div>
+          <Routes>
+            <Route exact path="/login" element={<LogIn/>}/>
+          </Routes>
+        </div>
+      </Layout>
   );
 }
 
-export default App;
+function LogIn() {
+  return <h2>Заходи</h2>;
+}
