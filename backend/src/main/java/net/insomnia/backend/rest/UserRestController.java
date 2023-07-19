f207d318e289ff8ed8c8e0ddec530dc77b695f7e
Matvey Rudinskiy <brokamp@mail.ru>
2023-07-19 19:51:32 +0300

    adding simple auth process

--- backend/src/main/java/net/insomnia/backend/rest/UserRestController.java
+++ backend/src/main/java/net/insomnia/backend/rest/UserRestController.java
@@ -1,4 +1,33 @@
 package net.insomnia.backend.rest;
 
+import net.insomnia.backend.models.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("api/v1/users")
 public class UserRestController {
+    private List<User> USERS = Stream.of(
+            new User(0L, "igand", "igand1953"),
+            new User(1L, "insomnic76", "123pass"),
+            new User(2L, "shibanuto73", "qwerty")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<User> getAll() {
+        return USERS;
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id) {
+        return USERS.stream().filter(user -> user.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
 }
